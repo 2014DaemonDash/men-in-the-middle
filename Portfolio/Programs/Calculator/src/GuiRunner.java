6bc4dd39c0f618e4c25eccd8963f5f79800e159f
DustinBrash <xrunner23@gmail.com>
2014-09-13 13:24:39 -0400

    lol

--- Portfolio/Programs/Calculator/src/GuiRunner.java
+++
@@ -1,9 +0,0 @@
-
-public class GuiRunner {
-    public static void main(String[] args){
-        //Creates a new CalcGUI named gui, centers it on the screen, and makes it visible
-        CalcGUI gui = new CalcGUI();
-        gui.setLocationRelativeTo(null);
-        gui.setVisible(true);
-    }
-}
